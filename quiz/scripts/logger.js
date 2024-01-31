bc4433cb0304bb6d56d1547bd7e8f6c38fd9d659
Zhaoqi Gao <gao.zhaoq@northeastern.edu>
2024-01-31 15:06:23 -0500

    changes

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,4 +1,14 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+const logger = {
+    logMsg: function () {
+        if()
+    }
+}
+
+const obj1 = {
+    errm
+}
+
 function logMsg() {
     console.log(`Error Massege: ${this.errMsg}`);
 }
