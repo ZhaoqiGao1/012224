5d7df519cf333a2dee09f0bdea41c297fcd33d1a
ZhaoqiGao1 <122407578+ZhaoqiGao1@users.noreply.github.com>
2024-02-05 16:16:52 -0500

    changes

--- quiz/scripts/map-deep.js
+++ quiz/scripts/map-deep.js
@@ -6,11 +6,11 @@ const originalMatrix = [
   ];
   
   const newMatrix = originalMatrix.map((row) => {
-    return row.sort();
+    return [...row].sort();
   });
   
   // Modifying the original matrix (changing the last element of the first row)
-  originalMatrix[0][2] = 3
+  originalMatrix[0][2] = 99
   
   // Displaying the original and new matrices
   console.log('Original Matrix (modified):', originalMatrix);
